421697f268c1a5cb52da95d9a797468a8bd7b079
yuki00yossi <yuki00yoshioka@gmail.com>
2023-03-05 01:54:13 +0900

    追加完了メッセージ表示機能追加 #1

--- resources/views/note/list.blade.php
+++ resources/views/note/list.blade.php
@@ -2,8 +2,20 @@
 @section('title', 'Notes')
 @section('somehead')
 <link rel="stylesheet" href="/css/note/list.css">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 @endsection
 @section('contents')
+{{-- メッセージアラート表示 --}}
+@if(null !== session('msg'))
+<script>
+Swal.fire({
+    icon: 'success',
+    title: '{{ session('msg') }}',
+    showConfirmButton: false,
+    timer: 1500
+});
+</script>
+@endif
 <div class="choose-game">
     <h2>Note List</h2>
     <div class="addNoteFormArea">
@@ -15,11 +27,14 @@
                 <input id="inputName" class="text-input" type="text" name="name" placeholder="note name">
                 <input class="btn-submit" type="submit" value="New Note">
             </div>
+            @if($errors->has('name'))
+            <span class="error-msg">{{ $errors->first('name') }}</span>
+            @endif 
         </form>
     </div>
     <div class="note-area">
         @foreach($notes as $note)
-        <div>{{ $note->name }}</div>
+        <div class="note-item"><a href="">{{ $note->name }}</a></div>
         @endforeach
     </div>
 </div>
